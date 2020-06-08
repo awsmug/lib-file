a63e04e7eb4b16a5fb101d8aadabec528b8d366d
Sven Wagener <sven@awesome.ug>
2020-06-08 14:12:40 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Awesome file loader
+
+Awesome file loader functionality. Easy and clean file loading.
+
+![](https://github.com/awsmug/lib-file-loader/workflows/PHPUnit/badge.svg)
+
+Version 1.0.0-beta-1
---
+++ src/File_Loader_Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AWSM\Lib_File_Loader;
+
+/**
+ * Class File_Loader_Exception.
+ *
+ * @package AWSM\Lib_Mail
+ *
+ * @since 1.0.0
+ */
+class File_Loader_Exception extends \Exception {
+}
